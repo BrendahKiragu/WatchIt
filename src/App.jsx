33720c750e3f7a09b07411db45b4feb1e5ce8562
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2025-03-18 23:21:51 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export default function App() {
+  return (
+    <main>
+      <header>
+        <h1>
+          Find <span className="text-gradient">Movies </span>you will Enjoy Without the Hassle
+        </h1>
+      </header>
+      <p>Search</p>
+    </main>
+  );
+}
+``
