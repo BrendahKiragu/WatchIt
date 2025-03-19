af125ed5b976aaa468db8192a0c5a014266c6550
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2025-03-20 00:26:35 +0300

    Created a function to fetch movies from the TMDB API. Used the useEffect hook to call the function. Established the api key to tmdb, the base url for search queries and api options

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,63 @@ import React from "react";
 import Search from "./components/search";
 import { useState, useEffect } from "react";
 
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_MOVIE_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
 export default function App() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  console.log("API Key:", API_KEY);
+
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(`Error fetching movies ${error}`);
+      setErrorMessage("Errorfetching movies. Please try again");
+    }
+  };
+  
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <div className="pattern">
         <div className="wrapper">
           <header>
-            <img src="./hero.png" alt="Hero background"/>
+            <img src="./hero.png" alt="Hero background" />
             <h1>
               Find <span className="text-gradient">Movies </span>you will Enjoy
               Without the Hassle
             </h1>
+
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </header>
-          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-          <h1 className="text-white">{searchTerm}</h1>
+
+          <section className="all-movies">
+            <h2>All movies</h2>
+          </section>
+          {/* <h1 className="text-white">{searchTerm}</h1> */}
         </div>
       </div>
       <p>Search</p>
