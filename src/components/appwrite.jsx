f0b5c793b191525010d860c189f6311e59e71d29
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2025-03-27 00:20:37 +0300

    The AppWriter function handles the logic for searching and updating documents in the database.

--- src/components/appwrite.jsx
+++ src/components/appwrite.jsx
@@ -1,7 +1,48 @@
+import { Client, Databases, Query, ID } from "appwrite";
+
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_COLLECTION_ID;
 const PROJECT_KEY = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 
-export const AppWrite = async () => {
-  console.log(DATABASE_ID, COLLECTION_ID, PROJECT_KEY);
+const client = new Client()
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(PROJECT_KEY);
+
+const database = new Databases(client);
+
+const AppWriter = async (searchTerm, movie) => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", searchTerm),
+    ]);
+
+    if (result.documents.length > 0) {
+      const doc = result.documents[0];
+
+      await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+        count: doc.count + 1,
+      });
+
+      return doc;
+    } else {
+      const newDoc = await database.createDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        ID.unique(),
+        {
+          searchTerm,
+          count: 1,
+          movie_id: movie.id,
+          poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+        }
+      );
+
+      return newDoc;
+    }
+  } catch (error) {
+    console.error(`Error fetching movies: ${error.message}`);
+    return null;
+  }
 };
+
+export default AppWriter;
