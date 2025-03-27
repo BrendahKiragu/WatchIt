8ab8e01e3cbbf2f237b3c284afd596826a03388d
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2025-03-28 00:24:35 +0300

    added onclick event to the search image and keydown event to search input for smooth scroll effect

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 
 const Search = ({ searchTerm, setSearchTerm }) => {
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      const targetSection = document.getElementById("all-movie");
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  };
+
+  // For the search image
+  const handleClick = (e) => {
+    e.preventDefault();
+    const targetSection = document.getElementById("all-movies");
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="search">
       <div>
-        <img src="./search.svg" alt="Search icon" />
         <input
           type="text"
           placeholder="Search through thousands of movies"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={handleKeyDown}
           id="search"
         />
+        <img src="./search.svg" alt="Search icon" onClick={handleClick} />
       </div>
     </div>
   );
